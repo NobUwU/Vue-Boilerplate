1ff35c96b7b4a4a05602eca8c95422b472dfe102
Nobu <nowobuwu@gmail.com>
2021-05-03 14:32:58 -0500

    some files

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  meta: {
+    title: 'Everfall',
+    description: 'Our focus at Everfall Games is to create unique and immersive single-player games. As a new studio, we are preparing for the production of LB-2; our non-linear horror game from which we hope to bring a fresh take on the horror genre.',
+    thumbnail: 'https://everfall-games.com/thumb.jpeg',
+    themeColor: '#ff2e2e',
+    url: 'https://everfall-games.com',
+  },
+  
+  port: Number(process.env.PORT) || 44444,
+}
